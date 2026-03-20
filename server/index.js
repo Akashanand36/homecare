00859e198a9a39e759cdb2a2097f94a89ab2c6ac
Akash <akashanand3615@gmail.com>
2026-03-20 13:36:52 +0530

    fixed backend deployment

--- server/index.js
+++ server/index.js
@@ -13,9 +13,12 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB error:", err));
+.catch(err => console.error("MongoDB error:", err.message));
 
 // API route
 app.get("/api/test", (req, res) => {
